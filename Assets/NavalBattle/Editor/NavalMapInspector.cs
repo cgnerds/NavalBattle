da62125cb1c2a730980296403515689b8880ef5c
cgnerds <cgnerds@gmail.com>
2018-10-08 10:16:29 +0800

    删除不相关的内容。

---
+++ Assets/NavalBattle/Editor/NavalMapInspector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using MapNavKit;
+
+[CustomEditor(typeof(NavalMap))]
+public class NavalMapInspector : MapNavHexaInspector
+{
+	// I need to do this so that all the tools of MapNav shows
+	// up in Inspector since it has a custom Inspector
+
+	protected override void OnEnable()
+	{
+		// Inform MapNav Inspector to use this custom node type
+		NodeType = typeof(NavalTile);
+
+		// Be sure to call the base OnEnable()
+		base.OnEnable();
+	}
+
+	public override void OnInspectorGUI()
+	{		
+		base.OnInspectorGUI();
+	}
+
+	protected override void OnSceneGUI()
+	{
+		base.OnSceneGUI();
+	}
+
+	// ------------------------------------------------------------------------------------------------------------
+}
